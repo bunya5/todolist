f7e44c4bcbbbd055410c4696e82ed454e28d1cc7
bunya5 <36207097+bunya5@users.noreply.github.com>
2021-06-24 00:18:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let addMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo');
+    let todoList = [];
+    if(localStorage.getItem("todo")){
+        todoList = JSON.parse(localStorage.getItem("todo"))
+        showMessages(); 
+        console.log(JSON.parse(localStorage.getItem("todo")))
+     }
 
+ addButton.addEventListener('click', function(){
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    };
+    todoList.push(newTodo);
+    localStorage.setItem("todo",JSON.stringify(todoList));
+    showMessages();  
+ });  
+ 
+ function showMessages (){
+    let showMessage = '';
+     todoList.forEach(function(item,i){
+     showMessage += `
+     <li>
+     <input type='checkbox' id='item_${i}'>
+     <label for='item_${i}'>${item.todo}<label>
+     </li>
+     `;
+     todo.innerHTML = showMessage;
+     
+     });
+ };
+ 
