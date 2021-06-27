25288a2caee3c44ba79927bc3cd5f654ce9a874d
bunya5 <36207097+bunya5@users.noreply.github.com>
2021-06-27 16:59:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,83 @@
 let addMessage = document.querySelector('.message'),
     addButton = document.querySelector('.add'),
-    todo = document.querySelector('.todo');
-    let todoList = [];
+    todo = document.querySelector('.todo'),
+    total = document.querySelector('.total');
+    let i = 0;
+let todoList = [];
 
-    //Чтобы нажималась кнопка по нажатию энтер в инпуте
-    addMessage.addEventListener('keyup', function(event){
-        event.preventDefault();
-        if(event.keyCode == 13){
-            addButton.click();
-        }
-    });
+
+if(todoList[0]!=undefined)
+localStorage.setItem('todo',JSON.stringify(todoList));
+   //  let elem = document.getElementsByTagName('h1')
+   //  console.log(elem)
+   //  setInterval(() => elem.hidden = !elem.hidden, 1000);
+
+//Чтобы нажималась кнопка по нажатию энтер в инпуте
+addMessage.addEventListener('keyup', function(event){
+   event.preventDefault();
+   if(event.keyCode == 13){
+   addButton.click();
+   }
+});
     
-    //если в LocalStorage есть данные по ключу todo то их записываем массиву todoList
-    if(localStorage.getItem("todo")){
-        todoList = JSON.parse(localStorage.getItem("todo"))
-        showMessages(); 
-     }
+//если в LocalStorage есть данные по ключу todo то их записываем массиву todoList
+window.onload = ()=> {
+   if(localStorage.getItem("todo")){
+   todoList = JSON.parse(localStorage.getItem("todo")) 
+   for (let index of todoList){
+   createDelTodo(index.todo)
+   i++
+      }
+   } 
+};
+
 // Прописываем действия по клику на кнопку Добавить
  addButton.addEventListener('click', function(){
-     if(addMessage.value==""){return}  //блокируем создание пустых дел
-    let newTodo = {
-        todo: addMessage.value,
-        checked: false,
-        important: false
-    }; // создаем объект с нашим делом
-    todoList.push(newTodo); // добавляем созданный объект в массив
-    localStorage.setItem("todo",JSON.stringify(todoList)); // отправляем наш массив в localStorage предварительно конвертирорав в формат строки
-    showMessages(); // показываем наши дела 
-    addMessage.value = ""; // очищаем поле инпута
+   if(addMessage.value==""){return}  //блокируем создание пустых дел
+   createDelTodo(addMessage.value)
+   changeList()
+   addMessage.value = ""; // очищаем поле инпута
+   i++
  });  
- 
- // пишем функцию для отображения наших дел на экран
- function showMessages (){
-    let showMessage = ''; 
-     todoList.forEach(function(item,i){ // перебираем наш массив и по каждому индексу используя уникальные свойства объекта создаем вёрстку
-     showMessage += `
-     <li>
-     <img class='deleteButton' id='${i}' src="trash.png">
-     <input type='checkbox' ${item.checked?'checked':''} id='item_${i}'>
-     <label for='item_${i}'>${item.todo}<label>
-     </li>
-     `;
-     });
-     todo.innerHTML = showMessage;
- };
 
- //Ниже, функция дает возможность сохранить значения чекбоксов
- todo.addEventListener("change", event => { // когда нажимается чекбокс
-    let numId = parseInt(event.target.getAttribute("id").replace(/\D+/g,"")); // у атрибута достаем числовое значение, это и есть индекс данного объекта в массиве
-    todoList[numId].checked = !todoList[numId].checked  // меняем значение чекед на противоположное
-     localStorage.setItem("todo",JSON.stringify(todoList)); // прописываем изменения в localStorage
+ function createDelTodo (value){
+   const li = document.createElement('li');
+   todo.appendChild(li)
+   li.className = 'li';
+   li.setAttribute('id',`${i}`)
+   li.textContent = value;
 
- } );
+   const delButton = document.createElement('button');
+   li.appendChild(delButton)
+   delButton.className = 'btnDel';
+   delButton.innerHTML = "<img class='trashImg' src='trash.png'>";
 
- //Ниже, функция дает возможность удалять дела
- todo.addEventListener("click", event => { 
-    if (event.target.getAttribute("class")== 'deleteButton'){  // если где произошло клик, соответсвует классу deleteButton  
-       let deleteId = event.target.getAttribute("id");  // то берем значение id элемента который вызвало событие
-       todoList.splice(deleteId,1);  // удаляем дело по индексу который соответствует id элемента 
-       localStorage.setItem("todo",JSON.stringify(todoList));  // прописываем изменения в localStorage
-       showMessages();  // выводим уже обновленный список дел
-    }
- });
+   const editButton = document.createElement('button');
+   li.appendChild(editButton)
+   editButton.className = 'btnEdit';
+   editButton.innerHTML = "<img class='editImg' src='edit.png'>";
+
+   delButton.addEventListener('click',() => {
+      todo.removeChild(li);
+   })
+   editButton.addEventListener('click',() => {
+      li.contentEditable = true;
+   })
+}
+todo.addEventListener("click", () => { 
+   changeList()
+});
+
+function changeList (){ 
+   let lis = document.querySelectorAll('li');//[2].getAttribute('class')
+   let list = [];
+   for (let index of lis){
+      let todo = {
+         todo: index.innerText,
+         checked: false
+      }
+    list.push(todo)
+   }
+   todoList = list;
+   localStorage.setItem('todo',JSON.stringify(todoList));
+}
