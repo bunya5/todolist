cea911b63323b6a167effebc5b563213434d8959
bunya5 <36207097+bunya5@users.noreply.github.com>
2021-06-28 17:13:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,8 @@ let addMessage = document.querySelector('.message'),
     let i = 0;
 let todoList = [];
 
-
 if(todoList[0]!=undefined)
 localStorage.setItem('todo',JSON.stringify(todoList));
-   //  let elem = document.getElementsByTagName('h1')
-   //  console.log(elem)
-   //  setInterval(() => elem.hidden = !elem.hidden, 1000);
 
 //Чтобы нажималась кнопка по нажатию энтер в инпуте
 addMessage.addEventListener('keyup', function(event){
@@ -20,7 +16,7 @@ addMessage.addEventListener('keyup', function(event){
    }
 });
     
-//если в LocalStorage есть данные по ключу todo то их записываем массиву todoList
+//при загрузки страницы, если в LocalStorage есть данные по ключу todo то их записываем массиву todoList
 window.onload = ()=> {
    if(localStorage.getItem("todo")){
    todoList = JSON.parse(localStorage.getItem("todo")) 
@@ -43,41 +39,68 @@ window.onload = ()=> {
  function createDelTodo (value){
    const li = document.createElement('li');
    todo.appendChild(li)
-   li.className = 'li';
-   li.setAttribute('id',`${i}`)
-   li.textContent = value;
 
-   const delButton = document.createElement('button');
+   const label = document.createElement('label');
+   li.appendChild(label)
+   label.textContent = value;
+
+   const editInput = document.createElement('input');
+   editInput.type = "text";
+   li.appendChild(editInput)
+   editInput.classList.add('editInput');
+
+   const delButton = document.createElement('img');
    li.appendChild(delButton)
-   delButton.className = 'btnDel';
-   delButton.innerHTML = "<img class='trashImg' src='trash.png'>";
+   delButton.classList.add('btnDel');
+   delButton.setAttribute('src','images/trash.svg')
 
-   const editButton = document.createElement('button');
+   const editButton = document.createElement('img');
    li.appendChild(editButton)
-   editButton.className = 'btnEdit';
-   editButton.innerHTML = "<img class='editImg' src='edit.png'>";
+   editButton.classList.add('btnEdit');
+   editButton.setAttribute('src','images/edit.svg')
 
    delButton.addEventListener('click',() => {
       todo.removeChild(li);
    })
-   editButton.addEventListener('click',() => {
-      li.contentEditable = true;
+
+   label.addEventListener('click',()=>{
+      label.classList.toggle('finished')
    })
+  
+   editButton.addEventListener('click',(event) => {
+      li.classList.toggle('editMode')
+      editButton.classList.toggle('editMode')
+      event.preventDefault();
+      containsClass = li.classList.contains('editMode');
+      if(containsClass){
+         editInput.value = label.textContent;
+         editInput.focus()
+
+         editInput.addEventListener('blur',()=> {
+           console.log(event.target)
+           li.classList.remove('editMode') 
+           label.textContent = editInput.value; 
+         })
+
+      } else {
+         label.textContent = editInput.value;
+      }
+   })
+   
 }
-todo.addEventListener("click", () => { 
-   changeList()
-});
+todo.addEventListener("click", () => changeList());
 
 function changeList (){ 
-   let lis = document.querySelectorAll('li');//[2].getAttribute('class')
-   let list = [];
-   for (let index of lis){
+   let liFromPage = document.querySelectorAll('li');       //[2].getAttribute('class')
+   let listOfLi = [];
+   for (let index of liFromPage){
       let todo = {
          todo: index.innerText,
          checked: false
       }
-    list.push(todo)
+      listOfLi.push(todo)
    }
-   todoList = list;
+   todoList = listOfLi;
    localStorage.setItem('todo',JSON.stringify(todoList));
 }
+
