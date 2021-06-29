fefac69141373c4c1c806568115bc308ed93d1a5
bunya5 <36207097+bunya5@users.noreply.github.com>
2021-06-29 16:45:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let addMessage = document.querySelector('.message'),
     addButton = document.querySelector('.add'),
     todo = document.querySelector('.todo'),
+    finishedTodo = document.querySelector('.finishedTodo'),
     total = document.querySelector('.total');
     let i = 0;
-let todoList = [];
+    let todoList = [];
+    const totalTodo = document.createElement('p')
+    total.appendChild(totalTodo)
+
+    let randomNum = Math.floor(Math.random() * 100);
+function genID(x) { return(x + (new Date).getTime());}
+    let randomId = genID(randomNum);
 
 if(todoList[0]!=undefined)
 localStorage.setItem('todo',JSON.stringify(todoList));
@@ -21,8 +28,7 @@ window.onload = ()=> {
    if(localStorage.getItem("todo")){
    todoList = JSON.parse(localStorage.getItem("todo")) 
    for (let index of todoList){
-   createDelTodo(index.todo)
-   i++
+   createDelTodo(index)
       }
    } 
 };
@@ -30,19 +36,29 @@ window.onload = ()=> {
 // Прописываем действия по клику на кнопку Добавить
  addButton.addEventListener('click', function(){
    if(addMessage.value==""){return}  //блокируем создание пустых дел
-   createDelTodo(addMessage.value)
-   changeList()
+   let todoObj = {
+      todo: addMessage.value,
+      id: genID(randomNum)
+   };
+   todoList.push(todoObj)
+   localStorage.setItem('todo',JSON.stringify(todoList));
+
+   createDelTodo(todoObj)
    addMessage.value = ""; // очищаем поле инпута
-   i++
  });  
 
- function createDelTodo (value){
+
+ function createDelTodo (todoObj){
+
+   totalTodo.innerHTML = `Итого: ${todoList.length} задач`;
+   
    const li = document.createElement('li');
    todo.appendChild(li)
+   li.setAttribute('data-index',todoObj.id)
 
    const label = document.createElement('label');
    li.appendChild(label)
-   label.textContent = value;
+   label.textContent = todoObj.todo;
 
    const editInput = document.createElement('input');
    editInput.type = "text";
@@ -53,11 +69,13 @@ window.onload = ()=> {
    li.appendChild(delButton)
    delButton.setAttribute('src','images/trash.svg')
    delButton.setAttribute('id','delBtn')
+   delButton.setAttribute('data-index',todoObj.id)
 
    const editButton = document.createElement('img');
    li.appendChild(editButton)
    editButton.setAttribute('src','images/edit.svg')
    editButton.setAttribute('id','editBtn')
+   editButton.setAttribute('data-index',todoObj.id)
 
    const saveButton = document.createElement('img');
    li.appendChild(saveButton)
@@ -65,21 +83,34 @@ window.onload = ()=> {
    saveButton.classList.add('hideSaveBtn');
 
    delButton.addEventListener('click',() => {
-      todo.removeChild(li);
+      for(let z = 0; z < todoList.length; z++) {
+         if(todoList[z].id == delButton.getAttribute('data-index')) {
+             todoList.splice(z, 1);
+             todo.removeChild(delButton.parentNode)
+             localStorage.setItem('todo',JSON.stringify(todoList));
+             totalTodo.innerHTML = `Итого: ${todoList.length} задач`;
+         }
+     }
    })
 
    label.addEventListener('click',()=>{
       label.classList.toggle('finished')
+      if(label.classList.contains('finished')){
+         finishedTodo.appendChild(li)
+      } else todo.appendChild(li)
    })
   
    editButton.addEventListener('click',(event) => {
       li.classList.toggle('editMode')
       editButton.classList.toggle('editButton')
       saveButton.setAttribute('id','saveBtn')
-      
-
       event.preventDefault();
       containsClass = li.classList.contains('editMode');
+
+      function saveChangedText(){ for(let z = 0; z < todoList.length; z++) {
+         if(todoList[z].id == editButton.getAttribute('data-index')) {
+             todoList[z].todo = label.textContent;}}}
+
       if(containsClass){
          editInput.value = label.textContent;
          editInput.focus()
@@ -91,36 +122,23 @@ window.onload = ()=> {
                editButton.classList.remove('editButton')
                saveButton.removeAttribute('id','saveBtn')
                label.textContent = editInput.value; 
+               saveChangedText()
             }
          });
 
          editInput.addEventListener('blur',()=> {
-           console.log(event.target)
            li.classList.remove('editMode') 
            editButton.classList.remove('editButton')
            saveButton.removeAttribute('id','saveBtn')
-           label.textContent = editInput.value; 
+           label.textContent = editInput.value;
+           saveChangedText() 
          })
 
       } else {
          label.textContent = editInput.value;
       }
    })
-   
-}
-todo.addEventListener("click", () => changeList());
 
-function changeList (){ 
-   let liFromPage = document.querySelectorAll('li');       //[2].getAttribute('class')
-   let listOfLi = [];
-   for (let index of liFromPage){
-      let todo = {
-         todo: index.innerText,
-         checked: false
-      }
-      listOfLi.push(todo)
-   }
-   todoList = listOfLi;
-   localStorage.setItem('todo',JSON.stringify(todoList));
 }
+
 
